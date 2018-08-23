35249a5f727f5fac2a92d9c3bdccfb1dc17e8196
Jan Sorgalla <jsorgalla@gmail.com>
2018-08-23 10:24:28 +0200

    Default msgPrefix to empty string

--- test/helper.js
+++ test/helper.js
@@ -1,11 +1,11 @@
-export function assertOpen(control, target, msgPrefix) {
+export function assertOpen(control, target, msgPrefix = '') {
     assert.equal(control.getAttribute('aria-expanded'), 'true', msgPrefix + 'Open: aria-expanded must be true');
     assert.equal(target.getAttribute('aria-hidden'), 'false', msgPrefix + 'Open: aria-hidden must be false');
     assert.equal(target.getAttribute('data-ctrly-opened'), '', msgPrefix + 'Open: data-ctrly-opened must be set and empty');
     assert.equal(target.getAttribute('tabindex'), '-1', msgPrefix + 'Open: tabindex must be -1');
 }
 
-export function assertClosed(control, target, msgPrefix) {
+export function assertClosed(control, target, msgPrefix = '') {
     assert.equal(control.getAttribute('aria-expanded'), 'false', msgPrefix + 'Closed: aria-expanded must be false');
     assert.equal(target.getAttribute('aria-hidden'), 'true', msgPrefix + 'Open: aria-expanded must be true');
     assert.isFalse(target.hasAttribute('data-ctrly-opened'), msgPrefix + 'Open: data-ctrly-opened must not be set');
